ac82f4e38a2928e6231150feadc4565217384806
Jason Powell <jpdevelopment@icloud.com>
2017-03-17 10:00:05 -0400

    changed to Express server

--- README.md
+++ README.md
@@ -1,17 +1,11 @@
 # Dudical Dungeons
 Created By Jason Powell
-Music: Vincenzo Salvia - The Awakening of the 1982 Golf GTD
-
-npm install && npm start
-open http://localhost:3000/
+Music: David Hasselhoff - True Survivor
 
 #Thanks
-A-Frame Boilerplate
-
+A-Frame Community
 Ryan Betts' A-Frame Daydream Controller Component
-
 Shane Hudson's "First Steps in VR"
-
 Fullstack Academy
 
 #Stackathon
---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+const volleyball = require('volleyball');
+app.use(volleyball);
+
+const path = require('path');
+app.use('/static', express.static(path.join(__dirname, '../browser/static')));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../browser/index.html'));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error');
+});
+
+app.listen(1337, () => {
+    console.log('The server is totally listen on port 1337, dude!');
+});
